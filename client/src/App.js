d70cf7ef387b63079c84ddaa026eee17ee52b416
Geoffrey You <107085735+geoffreynyc@users.noreply.github.com>
2023-07-18 23:27:10 -0400

    added ability to show flashcards

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,60 @@
+import { useState, useEffect } from "react";
+
 function App() {
-  return <div>React server</div>;
+  const [flashcards, setFlashcards] = useState([]);
+  const [title, setTitle] = useState("");
+
+  const fetchFlashcards = async () => {
+    const response = await fetch("http://localhost:5000/flashcard");
+    const newFlashcards = await response.json();
+    setFlashcards(newFlashcards);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:5000/flashcard", {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setTitle("");
+    fetchFlashcards();
+  };
+
+  useEffect(() => {
+    fetchFlashcards();
+  }, []);
+
+  const handleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>StudyBuddy</h1>
+      </header>
+      <div className="flashcards">
+        {flashcards.map((flashcard) => (
+          <li key={flashcard._id}>{flashcard.title}</li>
+        ))}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="card">Card title</label>
+        <input
+          value={title}
+          onChange={handleChange}
+          type="text"
+          id="card"
+        ></input>
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./App.css";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
